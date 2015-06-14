eba7ed144e3e6236541be006dfd0e57ef62529dc
Balaji Marisetti <bmarisetti@caviumnetworks.com>
2015-06-14 19:30:53 +0530

    code refactored

--- cdefines.py
+++ cdefines.py
@@ -1,61 +1,66 @@
 #! /usr/bin/python3
 
 from pathlib import Path
+from prompt_toolkit.shortcuts import get_input
+from prompt_toolkit.history import History
+import sys
 import re
-#import pdb
 
-defstart = re.compile(r'#[ \t]*define[ \t(]')
-tokentest = re.compile(r'([ \t]*)##([ \t]*)')
-stripchars = r"[()[\]{}.,?:;+\-=<>*^~|&%!/ \t\n\\]+"
-defs = []
+macro_start = re.compile(r'#[ \t]*define[ \t(]')
+token_pattern = re.compile(r'([ \t]*)##([ \t]*)')
+strip_chars = r"[()[\]{}.,?:;+\-=<>*^~|&%!/ \t\n\\]+"
+macro_list = []
 
-class Defline:
-    def __init__(self, expr, params, value, filename, lno):
+class Macro:
+    def __init__(self, expr, params, body, filename, lineno):
         self.expr = expr
         self.params = [x.strip() for x in params] if params else []
-        self.value = value
+        self.body = body
         self.filename = filename
-        self.lno = lno
+        self.lineno = lineno
         self.regex_matchers = None
 
     def __str__(self):
         return "#define %s%s %s\n%s:%d\n" % (self.expr,
                 ("(%s)" % ",".join(self.params)) if self.params is not None else "",
-                self.value if self.value else "" , self.filename, self.lno)
+                self.body if self.body else "" , self.filename, self.lineno)
 
     def has_params(self):
         True if self.params else False
     
     def has_paste_tokens(self):
-        True if "##" in value else False
+        True if "##" in self.body else False
    
     def get_regex_matchers(self):
+        """replaces macro-parameters in the macro-body
+        with regex pattern(r'\w*') and creates match object
+        """
         if self.regex_matchers is not None:
             return self.regex_matchers
-        if (not self.params) or (not self.value) or ("##" not in self.value):
+        if (not self.params) or (not self.body) or ("##" not in self.body):
+            #no token-paste operators, no regex matchers
             self.regex_matchers = []
             return self.regex_matchers
-        temp_value = self.value
-
+        temp_value = self.body
         tlen = 0
         end_index = 0
-        match = tokentest.search(self.value)
+        #search for token pasting operators and remove any white space on either side
+        match = token_pattern.search(self.body)
         while match:
             mstr = match.group()
-            tlen = len(mstr)
+            tlen = len(mstr) #len("##") + possible extra spaces
             temp_value = temp_value[:end_index] + temp_value[end_index:].replace(mstr, "##")
             end_index += (match.end()-tlen+2)
-            match = tokentest.search(temp_value[end_index:])
-        tokens = [x for x in re.split(stripchars,temp_value)]
-        newtokens = []
-        #pdb.set_trace()
+            match = token_pattern.search(temp_value[end_index:])
+        tokens = [x for x in re.split(strip_chars,temp_value)]
+        pattern_string_list = []
         for x in tokens:
-            if "##" not in x:
+            if ("##" not in x) or x.startswith("##"):
                 continue
             x = x.split("#")
-            _tmp = "".join([r"\w*" if n in self.params else n for n in x])
-            newtokens.append(_tmp)
-        self.regex_matchers = [re.compile(x) for x in newtokens]
+            regex_pattern_string = "".join([r"\w*" if n in self.params else n for n in x])
+            pattern_string_list.append(regex_pattern_string)
+        self.regex_matchers = [re.compile(x) for x in pattern_string_list]
         return self.regex_matchers
 
 
@@ -63,48 +68,50 @@ def build_defs(p="."):
     file_pattern="*.[ch]"
     if not Path(p).exists(): 
         raise Exception("Path doesn't exist")
-    global defs
-    defs.clear()
+    global macro_list
+    macro_list.clear()
     for fl in map(str, Path(p).rglob(file_pattern)):
         with open(fl, errors='ignore') as f:
-            defline = None 
+            macrostr = None 
             lineno = None
             for (lno, line) in enumerate(f, start=1):
                 line = line.strip()
-                if defstart.match(line):
-                    assert defline is None
+                if macro_start.match(line):
+                    assert macrostr is None
                     assert lineno is None
                     if line.endswith("\\"):
-                        defline = line+"\n"
+                        macrostr = line+"\n"
                         lineno = lno
                     else:
-                        obj = split_n_save_macro(line, fl, lno)
+                        obj = parse_macro(line, fl, lno)
                         if obj:
-                            defs.append(obj)
+                            macro_list.append(obj)
                         else:
                             print_err(line, fl, lno)
-                elif defline:
-                    assert isinstance(defline, str)
+                elif macrostr:
+                    assert isinstance(macrostr, str)
                     assert isinstance(lineno, int)
                     assert lineno is not None
-                    defline += line
+                    macrostr += line
                     if line.endswith("\\"):
-                        defline += "\n"
+                        macrostr += "\n"
                     else:
-                        obj = split_n_save_macro(defline, fl, lineno)
+                        obj = parse_macro(macrostr, fl, lineno)
                         if obj:
-                            defs.append(obj)
+                            macro_list.append(obj)
                         else:
-                            print_err(defline, fl, lineno)
-                        defline = None
+                            print_err(macrostr, fl, lineno)
+                        macrostr = None
                         lineno = None
                 else:
                     assert lineno is None
 
-def print_err(macrostr, filename, lno):
-        print("%s:%d:Unable to parse\n%s" % (filename, lno, macrostr))
 
-def split_n_save_macro(macrostr, filename, lno):
+def print_err(macrostr, filename, lineno):
+        print("%s:%d:Unable to parse\n%s" % (filename, lineno, macrostr))
+
+
+def parse_macro(macrostr, filename, lineno):
     '''The macro string should not be written in exotic ways
     Not all valid strings are recognized.
     e.g /* */# /**/define AB BA
@@ -142,8 +149,8 @@ def split_n_save_macro(macrostr, filename, lno):
             elif c == ")":
                 paren -= 1
                 if paren == 0:
-                    return Defline(macrostr[:par_index], macrostr[par_index+1:index].split(","),
-                                                     macrostr[index+1:].lstrip(), filename, lno)
+                    return Macro(macrostr[:par_index], macrostr[par_index+1:index].split(","),
+                                                     macrostr[index+1:].lstrip(), filename, lineno)
         else:
             if c == "(":
                 par_index = index
@@ -152,16 +159,17 @@ def split_n_save_macro(macrostr, filename, lno):
                 #raise Exception("Cannot parse string: %s" % macrostr)
                 return None
             elif c in " \t\n":
-                return Defline(macrostr[:index], None,  macrostr[index+1:].lstrip(), filename, lno)
+                return Macro(macrostr[:index], None,  macrostr[index+1:].lstrip(), filename, lineno)
     if paren > 0:
         #raise Exception("Malformed macro string: %s" % macrostr)
         return None
     else:
-        return Defline(macrostr, None, None, filename, lno)
+        return Macro(macrostr, None, None, filename, lineno)
+
 
 def get_def(defstr):
     deflist = []
-    for x in defs:
+    for x in macro_list:
         if defstr == x.expr:
             deflist.append(x)
     return deflist
@@ -169,39 +177,41 @@ def get_def(defstr):
 
 def main(source_path):
     build_defs(source_path)
-    print("total deflines:%d" % len(defs))
+    print("Total Macros:%d" % len(macro_list))
     history = History()
     while True:
         try:
-            answer = get_input('Def: ', history=history).lstrip()
+            command_str = get_input('Def: ', history=history).lstrip()
         except EOFError:
             break 
-        if answer == "":
-            continue
 
-        temp_list = answer.split(None, 1)
+        if command_str == "":
+            continue
+        temp_list = command_str.split(None, 1)
         command = temp_list[0]
         args = temp_list[1] if len(temp_list) == 2 else None
-        if command == "head" and args:
+        if command == "expr" and args:
             x = get_def(args)
             for y in x:
                 print(y)
-        elif command == "tail" and args:
-            for (i,x) in enumerate(defs):
+        elif command == "body" and args:
+            for (i,x) in enumerate(macro_list):
                 y = x.get_regex_matchers()
                 for z in y:
                     match = z.search(args)
                     if match:
-                        print("Defline:%d" % i)
+                        print("Macro:%d" % i)
                         print(x)
                         break
+        elif command == "list":
+            if args:
+                for x in macro_list:
+                    if args in x:
+                        print(x)
+            else:
+                for x in macro_list:
+                    print(x)
             
-
-#from prompt_toolkit.contrib.completers import PathCompleter
-#from prompt_toolkit.contrib.completers import WordCompleter
-from prompt_toolkit.shortcuts import get_input
-from prompt_toolkit.history import History
-import sys
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
